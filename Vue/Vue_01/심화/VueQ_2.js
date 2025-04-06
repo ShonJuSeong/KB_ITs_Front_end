545cf728037eb3a437943cfb0ff51d01828e80d7
Shon Juseong <sow7722@gmail.com>
2025-04-06 16:44:18 +0900

    Create VueQ_2.js

---
+++ Vue/Vue_01/심화/VueQ_2.js
@@ -0,0 +1,8 @@
+// 전개연산자를 사용하여 아래 결과가 나오도록 배열을 정의하세요.
+// [ 100, 200, 300 ]
+// [ 'hello', 100, 200, 300, 'world' ]
+
+let arr1 = [100, 200, 300];
+let arr2 = [`hello`, ...arr1, , `world`];
+console.log(arr1);
+console.log(arr2);
